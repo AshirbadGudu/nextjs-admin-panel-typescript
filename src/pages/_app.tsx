aa44ce91b6026f243c5e5a015440c18ed61068cd
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-05-14 12:31:27 +0530

    nprogress implemented in the application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AppContextProvider } from 'contexts'
+import Router from 'next/router'
+import nProgress from 'nprogress'
+
+Router.events.on('routeChangeStart', nProgress.start)
+Router.events.on('routeChangeError', nProgress.done)
+Router.events.on('routeChangeComplete', nProgress.done)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,12 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/layouts/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
+        primary: '#8e24aa',
         facebook: '#3b5999',
         twitter: '#55acee',
         linkedin: '#0077b5',
