d8297133af8391c4de0722f8470d7200ea8d52a9
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-05-14 17:00:07 +0530

    _doc file created with material icons link tag

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
