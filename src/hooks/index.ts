ec35ad9bf1e432b15f54611c871cdb6abfdb9729
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-05-14 16:45:52 +0530

    useFetch hook created

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useIsMounted } from './useIsMounted'
 export { default as useMenuItems } from './useMenuItems'
+export { default as useFetch } from './useFetch'
---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,70 @@
+import { useEffect, useReducer } from 'react'
+import { getArrFromNestedSnap, getArrFromSnap } from '@ashirbad/js-core'
+import useIsMounted from './useIsMounted'
+import { database } from 'configs'
+
+type Options = {
+  needArray?: boolean
+  needNested?: boolean
+  filter?: (data: any) => boolean
+}
+
+type dataType = {
+  data: any
+  isLoading: boolean
+}
+type reducerActionType = {
+  type: 'needArray' | 'needNested' | 'filter' | 'raw'
+  payload: {
+    snap: any
+  }
+}
+
+const dataReducer = (state: dataType, action: reducerActionType): dataType => {
+  switch (action.type) {
+    case 'needArray':
+      return {
+        data: getArrFromSnap(action.payload.snap),
+        isLoading: false,
+      }
+    case 'needNested':
+      return {
+        data: getArrFromNestedSnap(action.payload.snap),
+        isLoading: false,
+      }
+    case 'raw':
+      return {
+        data: action.payload.snap.val(),
+        isLoading: false,
+      }
+    default:
+      return state
+  }
+}
+
+export default function useFetch<T>(path: string, options?: Options) {
+  const needArray = options?.needArray === false ? false : true
+  const needNested = Boolean(options?.needNested)
+  const filter = options?.filter || (() => true)
+  const [state, dispatch] = useReducer(dataReducer, {
+    data: null,
+    isLoading: true,
+  })
+  const isMounted = useIsMounted()
+
+  useEffect(() => {
+    database.ref(path).on('value', (snap) => {
+      isMounted.current &&
+        dispatch({
+          type: needArray ? 'needArray' : needNested ? 'needNested' : 'raw',
+          payload: { snap },
+        })
+    })
+  }, [isMounted, needArray, needNested, path])
+  type ReturnType = [T, boolean]
+  const returnType: ReturnType = [
+    needArray ? state.data?.filter(filter) : state.data,
+    state.isLoading,
+  ]
+  return returnType
+}
