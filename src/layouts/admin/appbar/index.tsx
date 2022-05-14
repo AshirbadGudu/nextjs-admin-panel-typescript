2ae10f1639f5bdc79fcb094e10d1126cb25f9f6e
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-05-14 17:43:12 +0530

    dynamic notifications count added

--- src/layouts/admin/appbar/index.tsx
+++ src/layouts/admin/appbar/index.tsx
@@ -1,15 +1,25 @@
 import { Badge } from '@mui/material'
 import { ICONS } from 'assets'
+import { useAppContext } from 'contexts'
+import { useFetch } from 'hooks'
 import Link from 'next/link'
+import { NotificationType } from 'types'
 import AccountMenu from './AccountMenu'
 export default function AppBar() {
+  const { user } = useAppContext()
+  const [notifications] = useFetch<NotificationType[]>(
+    `notifications/${user?.uid}`,
+    {
+      filter: (notification: NotificationType) => !notification.isRead,
+    }
+  )
   return (
     <>
       <header className={`h-16 bg-white shadow`}>
         <div className="flex h-16 items-center justify-between px-4">
           <h1 className="hidden text-xl lg:block">Admin Panel</h1>
           <div className="flex items-center gap-6">
-            <Badge badgeContent={10} color="warning">
+            <Badge badgeContent={notifications?.length || 0} color="warning">
               <Link href="/admin/notifications">
                 <a className="cursor-pointer rounded-lg bg-amber-100 p-2">
                   <ICONS.Notification className="h-6 w-6 text-amber-700" />
